299cbc38a8068d103225d9ce5e28917a23a2bf91
nicoleromain <nmromain@svsu.edu>
2017-01-30 13:39:08 -0500

    Added Join_Game screen page

---
+++ app/src/main/java/edu/svsu/tacops/tacops/JoinGame.java
@@ -0,0 +1,13 @@
+package edu.svsu.tacops.tacops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_game);
+    }
+}
