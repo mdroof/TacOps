7d064b5c08c896b96a6c8709064e44c3cf816313
mdroof <mdroof@svsu.edu>
2017-01-31 00:59:40 -0500

    Adds a sign out button on the MainMenu layout with functionality. Hid other Firebase components from Authenication layout by setting visibility property to invisible

--- app/src/main/java/edu/svsu/tacops/tacops/MainMenu.java
+++ app/src/main/java/edu/svsu/tacops/tacops/MainMenu.java
@@ -1,25 +1,84 @@
 package edu.svsu.tacops.tacops;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainMenu extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainMenu extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     Button create_game_button;
     Button join_game_button;
     Button settings_button;
+    Button sign_out_button;
+
+    private static final String TAG = "MainMenuActivity";
+
+    private GoogleApiClient mGoogleApiClient;
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+        // [END auth_state_listener]
+
         create_game_button = (Button) findViewById(R.id.create_game_button);
         join_game_button = (Button) findViewById(R.id.join_game_button);
         settings_button = (Button) findViewById(R.id.settings_button);
+        sign_out_button = (Button) findViewById(R.id.sign_out);
 
         create_game_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -48,6 +107,37 @@ public class MainMenu extends AppCompatActivity {
                 */
             }
         });
+
+        sign_out_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                // Firebase sign out
+                mAuth.signOut();
+
+                // Google sign out
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(@NonNull Status status) {
+                                //updateUI(null);
+                            }
+                        });
+                Toast.makeText(v.getContext(), "You have been signed out.",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(v.getContext(), Authentication.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
