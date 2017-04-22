d6b72b93c16210244d122df7834b895635b44114
enandersson <swedishtwin@hotmail.com>
2017-04-21 22:57:07 -0400

    Changed GameScreen Layout

--- app/src/main/java/edu/svsu/tacops/tacops/gameScreen.java
+++ app/src/main/java/edu/svsu/tacops/tacops/gameScreen.java
@@ -11,29 +11,29 @@ import android.util.TypedValue;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.graphics.Color;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.tacops.Game;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameScreen extends AppCompatActivity {
 
-    Button btn1;
-    Button btn2;
-    Button btn3;
-    Button btn4;
-    Button btn5;
-    Button btn6;
-    TextView textTitle;
-    TextView scoreText;
-    TextView textTitle1;
-    TextView scoreText1;
-    TextView timer; // Added timer
-    int counter = 0;
+    TextView tv1;
+    TextView tv2;
+    TextView tv3;
+    TextView tv4;
     int counter1 = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+    int counter4 = 0;
 
-    Button buttonResetScore;
-    Button buttonStartTime;                 // clicking this button will start time count down
+    Button buttonStartTime;                 // clicking this button will enable the score listeners
+    Button buttonPauseTime;                 // Yet to be implemented
+    Button buttonEndMission;                // Yet to be implemented
     TextView textViewShowTime;              // will show the time
     CountDownTimer countDownTimer;          // built in android class CountDownTimer
     long totalTimeCountInMilliseconds;      // total count down time in milliseconds
@@ -55,139 +55,164 @@ public class GameScreen extends AppCompatActivity {
         Game currentGame = (Game)intent.getSerializableExtra("game");
         game_uid = (String)intent.getSerializableExtra("game_uid");     //Setting the Game_uid for Firebase Reference
 
-
         alert_manager = AlertManager.getInstance(game_uid);             //Making new AlertManager global instance
 
         //Set Firebase reference
         mDatabase = FirebaseDatabase.getInstance().getReference("game_list/" + game_uid);
 
-        btn1 = (Button)findViewById(R.id.addButton);
-        btn2 = (Button)findViewById(R.id.subtractButton);
-        btn3 = (Button)findViewById(R.id.resetButton);
-        btn4 = (Button)findViewById(R.id.addButton1);
-        btn5 = (Button)findViewById(R.id.subtractButton1);
-        btn6 = (Button)findViewById(R.id.resetButton1);
-        timer = (TextView)findViewById(R.id.tvTimeCount); // Added timer view id
-        scoreText = (TextView)findViewById(R.id.scoreText);
-        textTitle = (TextView)findViewById(R.id.myTextTitle);
-        scoreText1 = (TextView)findViewById(R.id.scoreText1);
-        textTitle1 = (TextView)findViewById(R.id.myTextTitle1);
-
+        getReferenceOfViews();
+        timerEvent(currentGame);// Starts and handles the timer
+        textViewShowTime.setTextAppearance(getApplicationContext(), R.style.normalText);
+        tv1.setTextAppearance(getApplicationContext(), R.style.normalText);
+        tv2.setTextAppearance(getApplicationContext(), R.style.normalText);
+        tv3.setTextAppearance(getApplicationContext(), R.style.normalText);
+        tv4.setTextAppearance(getApplicationContext(), R.style.normalText);
         // change font size of the text
-        textTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-        textTitle1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-        timer.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-        scoreText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-        scoreText1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+//        textViewShowTime.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
 
-        getReferenceOfViews ();
-        setActionListeners ();
-
-        Double timeLimit = currentGame.getTime_limit();
-        timer.setText(timeLimit.toString()); // Sets timer text to Game's time attribute
-
-        totalTimeCountInMilliseconds = timeLimit.longValue() * 60000;
-        timeBlinkInMilliseconds = 1 * 60000;
+        setActionListeners(currentGame);
     }
 
-    private void setActionListeners() {
+    private void setActionListeners(Game currentGame) {
+        final int scoreLimit = currentGame.getScore_limit();
+        // Button listeners are enabled when the start(pause) button is pressed
         buttonStartTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                textViewShowTime.setTextAppearance(getApplicationContext(), R.style.normalText);
-
-                countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 500) {
-                    // 500 means, onTick function will be called at every 500 milliseconds
-
-                    @Override
-                    public void onTick(long leftTimeInMilliseconds) {
-                        long seconds = leftTimeInMilliseconds / 1000;
-
-                        if (leftTimeInMilliseconds < timeBlinkInMilliseconds) {
-                            textViewShowTime.setTextAppearance(getApplicationContext(), R.style.blinkText);
-                            // change the style of the textview .. giving a red alert style
-
-                            if (blink) {
-                                textViewShowTime.setVisibility(View.VISIBLE);
-                                // if blink is true, textview will be visible
-                            } else {
-                                textViewShowTime.setVisibility(View.VISIBLE);
-                            }
-
-                            blink = !blink;         // toggle the value of blink
-                        }
-
-                        textViewShowTime.setText(String.format("%02d", seconds / 60) + ":" + String.format("%02d", seconds % 60));
-                        // format the textview to show the easily readable format
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        // this function will be called when the timecount is finished
-                        textViewShowTime.setText("Times up!");
-                        textViewShowTime.setVisibility(View.VISIBLE);
-                    }
-
-                }.start();
-            }
-        });
-        buttonResetScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //—set on click listeners on the buttons—–
-                btn1.setOnClickListener(this);
-                btn2.setOnClickListener(this);
-                btn3.setOnClickListener(this);
-                btn4.setOnClickListener(this);
-                btn5.setOnClickListener(this);
-                btn6.setOnClickListener(this);
+                tv1.setOnClickListener(this); // team1 +1
+                tv2.setOnClickListener(this); // team2 +1
+                tv3.setOnClickListener(this); // team3 +1
+                tv4.setOnClickListener(this); // team4 +1
 
-                if (view == btn1) {
-                    counter++;
-                    scoreText.setText(Integer.toString(counter));
-                    scoreText.setBackgroundColor(Color.CYAN);
-                    alert_manager.sendAlert("Team 1", "scored a point!");
-                }
-                if (view == btn2) {
-                    counter--;
-                    scoreText.setText(Integer.toString(counter));
-                    scoreText.setBackgroundColor(Color.GREEN);
-                    alert_manager.sendAlert("Team 1", "lost a point!");
-                }
-
-                if (view == btn3) {
-                    counter = 0;
-                    scoreText.setText(Integer.toString(counter));
-                    scoreText.setBackgroundColor(Color.RED);
-                    alert_manager.sendAlert("Team 1", "score was reset!");
-                }
-                if (view == btn4) {
+                if (view == tv1) {
                     counter1++;
-                    scoreText1.setText(Integer.toString(counter1));
-                    scoreText1.setBackgroundColor(Color.CYAN);
-                    alert_manager.sendAlert("Team 2", "scored a point!");
+                    tv1.setText(Integer.toString(counter1));
+                    //scoreText.setBackgroundColor(Color.RED);
+                    alert_manager.sendAlert("Team 1", "Scored a point!");
                 }
-                if (view == btn5) {
-                    counter1--;
-                    scoreText1.setText(Integer.toString(counter1));
-                    scoreText1.setBackgroundColor(Color.GREEN);
-                    alert_manager.sendAlert("Team 2", "lost a point!");
+                if (view == tv2) {
+                    counter2++;
+                    tv2.setText(Integer.toString(counter2));
+                    //scoreText.setBackgroundColor(Color.BLUE);
+                    alert_manager.sendAlert("Team 2", "Scored a point!");
                 }
 
-                if (view == btn6) {
-                    counter1 = 0;
-                    scoreText1.setText(Integer.toString(counter1));
-                    scoreText1.setBackgroundColor(Color.RED);
-                    alert_manager.sendAlert("Team 2", "score was reset!");
+                if (view == tv3) {
+                    counter3++;
+                    tv3.setText(Integer.toString(counter3));
+                    //scoreText.setBackgroundColor(Color.GREEN);
+                    alert_manager.sendAlert("Team 3", "Scored a point!");
+                }
+                if (view == tv4) {
+                    counter4++;
+                    tv4.setText(Integer.toString(counter4));
+                    //scoreText1.setBackgroundColor(Color.Yellow);
+                    alert_manager.sendAlert("Team 4", "Scored a point!");
+                }
+
+                //End Game if score limit reached
+                if (counter1 == scoreLimit) {
+                    //End game
+                    tv1.setText("WIN");
+                    System.out.println("Score Limit Reached");
+                }else if ((counter2 == scoreLimit) ){
+                    //End game
+                    tv2.setText("WIN");
+                    System.out.println("Score Limit Reached");
+                }else if ((counter3 == scoreLimit) ){
+                    //End Game
+                    tv3.setText("WIN");
+                    System.out.println("Score Limit Reached");
+                }else if ((counter4 == scoreLimit) ){
+                    //End Game
+                    tv4.setText("WIN");
+                    System.out.println("Score Limit Reached");
                 }
             }
         });
     }
 
-    private void getReferenceOfViews() {
-        buttonResetScore = (Button) findViewById(R.id.resetButton);
-        buttonStartTime = (Button) findViewById(R.id.btnStartTime);
-        textViewShowTime = (TextView) findViewById(R.id.tvTimeCount);
+    private void OnHold(){
+        // Code for onHold
+        int delay = 5000; // delay for 5 sec.
+        int period = 1000; // repeat every sec.
+        int count = 0;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+            public void run()
+            {
+                // Your code
+                //count++;
+            }
+        }, delay, period);
     }
-}
+
+ /*   private void EndGameListener(){
+        buttonEndMission.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void OnClick(View view){
+                System.out.println();
+            }
+        });    }*/
+
+
+
+    /**
+     * Runs the timer as soon as the GameScreen is created
+     */
+    public void timerEvent(Game currentGame){
+        Double timeLimit = currentGame.getTime_limit(); // Gets time limit set from game settings
+        textViewShowTime.setText(timeLimit.toString()); // Sets timer text to Game's time attribute
+
+        totalTimeCountInMilliseconds = timeLimit.longValue() * 60000;
+        timeBlinkInMilliseconds = 1 * 60000;
+        countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 500) {
+            // 500 means, onTick function will be called at every 500 milliseconds
+
+            @Override
+            public void onTick(long leftTimeInMilliseconds) {
+                long seconds = leftTimeInMilliseconds / 1000;
+
+                if (leftTimeInMilliseconds < timeBlinkInMilliseconds) {
+                    textViewShowTime.setTextAppearance(getApplicationContext(), R.style.blinkText);
+                    // change the style of the textview .. giving a red alert style
+
+                    if (blink) {
+                        textViewShowTime.setVisibility(View.VISIBLE);
+                        // if blink is true, textview will be visible
+                    } else {
+                        textViewShowTime.setVisibility(View.VISIBLE);
+                    }
+
+                    blink = !blink;         // toggle the value of blink
+                }
+
+                textViewShowTime.setText(String.format("%02d", seconds / 60) + ":" + String.format("%02d", seconds % 60));
+                // format the textview to show the easily readable format
+            }
+
+            @Override
+            public void onFinish() {
+                // this function will be called when the timeCount is finished
+                textViewShowTime.setText("Times up!");
+                textViewShowTime.setVisibility(View.VISIBLE);
+            }
+
+        }.start();
+    }
+
+    private void getReferenceOfViews() {
+        textViewShowTime = (TextView) findViewById(R.id.tvTimeCount);
+        tv1 = (TextView)findViewById(R.id.tvScoreCount1);
+        tv2 = (TextView) findViewById(R.id.tvScoreCount2);
+        tv3 = (TextView) findViewById(R.id.tvScoreCount3);
+        tv4 = (TextView) findViewById(R.id.tvScoreCount4);
+        buttonStartTime = (Button) findViewById(R.id.btnStart);
+        buttonPauseTime = (Button) findViewById(R.id.btnPause);
+        buttonEndMission = (Button) findViewById(R.id.btnEnd);
+    }
+} // End GameScreen
